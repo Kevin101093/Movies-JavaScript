905a3790fc54c74b4da6ce2c3bbcf5967c3dde86
Kevin101093 <kevin101093@hotmail.com>
2023-02-16 18:15:45 -0600

    Mostrar la lista de peliculas en tendencia

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
-console.log('Esta es mi llave' + key);
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+  console.log(movies);
+
+  movies.forEach((movie) => {
+    const trendingPreviewMoviesContainer = document.querySelector(
+      '#trendingPreview .trendingPreview-movieList'
+    );
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+    movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+getTrendingMoviesPreview();
